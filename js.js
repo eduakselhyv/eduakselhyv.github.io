094362ead9de85dcb07cbf324cf9e299f0458b19
Akseli Hyvönen <142724134+eduakselhyv@users.noreply.github.com>
2024-01-09 13:19:43 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,9 +1,12 @@
 let score = parseInt(localStorage.getItem('savedscore')) || 0;
-let passive = 0;
-let modifier = 1;
-let pcprice = 20;
-let bcprice = 10;
+let passive = parseInt(localStorage.getItem('savedpassive')) || 0;
+let modifier = parseInt(localStorage.getItem('savedmodifier')) || 1;
+let pcprice = parseInt(localStorage.getItem('savedpassiveprice')) || 20;
+let bcprice = parseInt(localStorage.getItem('savedmodifierprice')) || 10;
+
 document.getElementById('score').innerHTML = score;
+document.getElementById('bcp').innerHTML = 'Price: ' + bcprice;
+document.getElementById('pcp').innerHTML = 'Price: ' + pcprice;
 
 function cookieclicked() {
     score += modifier;
@@ -17,6 +20,9 @@ function bcc() {
         modifier += 1;
         bcprice *= 1.5;
         bcprice = Math.round(bcprice);
+        localStorage.setItem("savedmodifier", modifier);
+        localStorage.setItem("savedmodifierprice", bcprice);
+        localStorage.setItem("savedscore", score);
     }
     document.getElementById('bcp').innerHTML = 'Price: ' + bcprice;
     document.getElementById('score').innerHTML = score;
@@ -28,6 +34,9 @@ function pcc() {
         passive += 1;
         pcprice *= 1.5;
         pcprice = Math.round(pcprice);
+        localStorage.setItem("savedpassive", passive);
+        localStorage.setItem("savedpassiveprice", pcprice);
+        localStorage.setItem("savedscore", score);
     }
     document.getElementById('pcp').innerHTML = 'Price: ' + pcprice;
     document.getElementById('score').innerHTML = score;
@@ -37,7 +46,26 @@ function passivecookies() {
     setInterval(() => {
         score += passive;
         document.getElementById('score').innerHTML = score;
+        localStorage.setItem("savedscore", score);
     }, 1000);
+}
+
+function reset() {
+    localStorage.setItem("savedscore", 0);
+    localStorage.setItem("savedpassive", 0);
+    localStorage.setItem("savedmodifier", 1);
+    localStorage.setItem("savedpassiveprice", 20);
+    localStorage.setItem("savedmodifierprice", 10);
+
+    score = 0;
+    passive = 0;
+    modifier = 1;
+    pcprice = 20;
+    bcprice = 10;
+
+    document.getElementById('score').innerHTML = score;
+    document.getElementById('bcp').innerHTML = 'Price: ' + bcprice;
+    document.getElementById('pcp').innerHTML = 'Price: ' + pcprice;
 }
 
 passivecookies()
